aff634fbe3c74a7f46dff8a39acbaa45aae73b19
HariharanPandurengan <hariharanpandurengan@gmail.com>
2024-10-04 23:21:58 +0530

    commit

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -2,9 +2,6 @@ import connectMongo from "../../../utils/connectMongo";
 import UserModel from "../../../models/userModel";
 
 export async function POST(req,res){
-    res.setHeader('Access-Control-Allow-Origin', '*'); 
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     try {
         await connectMongo();
         const loginDetails = await req.json();
