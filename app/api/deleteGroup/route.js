f887455a7fe8233a0d2c8cf027559bcee0202d42
HariharanPandurengan <hariharanpandurengan@gmail.com>
2024-10-04 23:16:02 +0530

    commit

--- app/api/deleteGroup/route.js
+++ app/api/deleteGroup/route.js
@@ -1,7 +1,3 @@
 import connectMongo from "../../../utils/connectMongo";
 import UserModel from "../../../models/userModel";
 import GroupChatModel from "../../../models/groupChatModel"
-
-// export async function GET(req,res){
-//   return '..'
-// }
