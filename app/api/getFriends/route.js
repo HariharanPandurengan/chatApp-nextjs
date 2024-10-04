e49b1f3ce8bcbc6af706299b9b67c9d162e5d33e
HariharanPandurengan <hariharanpandurengan@gmail.com>
2024-10-04 22:22:35 +0530

    commit

--- app/api/getFriends/route.js
+++ app/api/getFriends/route.js
@@ -3,8 +3,8 @@ import FriendsModel from "../../../models/friendsModel"
 
   export async function GET(req,res){
     res.setHeader('Access-Control-Allow-Origin', '*'); // You can specify the exact domain instead of '*'
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     try {
         await connectMongo();
         
