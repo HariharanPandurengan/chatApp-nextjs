63982c752466d67abc70d7877d2cbd70790578be
HariharanPandurengan <hariharanpandurengan@gmail.com>
2024-10-07 14:37:27 +0530

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,6 +28,7 @@ const config: Config = {
       'spin-once': 'spinOnce 1s ease-in-out 1', // 1 rotation in 1 second
       'spin-once2': 'spinOnce2 1s ease forwards 1s', // 1 rotation in 1 second
       'show-element': 'showElement 1s forwards 1s',
+      'justify-content-animation': 'justifyAnimation 1s ease-in-out forwards',
     },
     keyframes: {
       spinOnce: {
@@ -35,13 +36,16 @@ const config: Config = {
         '100%': { transform: 'rotate(360deg)' },
       },
       spinOnce2: {
-        '0%': { opacity: '0',transform: 'rotate(360deg)' },
-        '1%': { opacity: '1' },
-        '100%': { opacity: '1',transform: 'rotate(0deg)' },
+        '0%': { transform: 'rotate(360deg)' },
+        '100%': { transform: 'rotate(0deg)' },
       },
       showElement: {
         '0%': { visibility: 'hidden', opacity: '0' },
         '100%': { visibility: 'visible', opacity: '1' },
+      },
+      justifyAnimation: {
+        '0%': { 'justify-content': 'space-between' },
+        '100%': { 'justify-content': 'center' },
       },
     },
   },
