b0029394862cba0934eecc62fc8ea7c1e3c3522e
HariharanPandurengan <hariharanpandurengan@gmail.com>
2024-10-11 03:27:21 +0530

    seenUpdate

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -175,68 +175,84 @@ export default function Chat() {
       });
     }
     return(
-      <div className="w-full bg-gradient-to-b from-yellow-200 to-white-100  min-h-screen sm:pt-1 p-0">
-        <div className="relative chat-div w-full">
-          <div className="sticky top-0 left-0 w-full flex items-center space-x-4 p-4 bg-gray-300 rounded mb-4">
-            <div className='flex items-center'>
-              <div className="w-12 h-12 rounded-full overflow-hidden me-2">
-                  <Image 
-                      src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-social-media-user-vector-default-avatar-profile-icon-social-media-user-vector-portrait-176194876.jpg" 
-                      alt="Profile Avatar"
-                      width={48}
-                      height={48}
-                  />
+        <div className="w-full bg-gradient-to-b from-yellow-200 to-white-100  min-h-screen sm:pt-1 p-0">
+            <div className="relative chat-div w-full">
+              <div className="sticky top-0 left-0 w-full flex items-center space-x-4 p-4 bg-gray-300 rounded mb-4">
+                <div className='flex items-center'>
+                    <div className="w-12 h-12 rounded-full overflow-hidden me-2">
+                          <Image 
+                              src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-social-media-user-vector-default-avatar-profile-icon-social-media-user-vector-portrait-176194876.jpg" 
+                              alt="Profile Avatar"
+                              width={48}
+                              height={48}
+                          />
+                    </div>
+                    <h2 className="text-lg font-medium text-black">{opposite_person}</h2>
+                </div>
+                    
+                <div className='text-center w-full'>
+                  <h5 className={onlineCheck ? 'text-green-500 font-bold bg-white w-1/3 sm:w-1/5 m-auto rounded' : 'text-red-600 font-bold bg-white w-1/3 sm:w-1/5 m-auto rounded'}>{onlineCheck ? 'Online' : 'Offline'}</h5>
+                </div>
               </div>
-              <h2 className="text-lg font-medium text-black">{opposite_person}</h2>
-            </div>
-              
-            <div className='text-center w-full'>
-              <h5 className={onlineCheck ? 'text-green-500 font-bold bg-white w-1/3 sm:w-1/5 m-auto rounded' : 'text-red-600 font-bold bg-white w-1/3 sm:w-1/5 m-auto rounded'}>{onlineCheck ? 'Online' : 'Offline'}</h5>
-            </div>
-          </div>
-      
-          <div className="chat-container" ref={chatContainerRef}>
-              {
-                 chat.length !== 0 && 
-                    chat.map((item) => {
-                      const messageContent = item.chat;
-                      const isSent = (item.from === user);
-                      return (
-                          <div key={item.id}> 
-                              {
-                                  isSent ? 
-                                  <div className="w-full flex justify-end items-center w-full">
-                                      {/* <small className="text-xs font-light">you</small> */}
-                                      <div key={item.id} className='message message-sent ms-2'>
-                                          <p className="text-xl font-medium">{messageContent}<small className="text-xs font-light ms-2">{item.mTime}</small></p>
-                                      </div>
-                                      {
-                                        item.seen ?
-                                          <p className='text-blue-500'>&#10004;</p>
-                                        :
-                                          <p className='text-gray-500'>&#10004;</p>
-                                      }
-                                  </div>
-                                  :
-                                  <div className="w-full flex justify-start items-center">
-                                      <div key={item.id} className='message message-received  ms-2'>
-                                          <p className="text-xl font-medium">{messageContent}<small className="text-xs font-light ms-2">{item.mTime}</small></p>
-                                      </div>
-                                      {/* <small className="text-xs font-light ms-2">{opposite_person}</small> */}
-                                  </div>
+            
+                <div className="chat-container z-20" ref={chatContainerRef}>
+                    {
+                       chat.length !== 0 && 
+                          chat.map((item,index) => {
+                            const decryptedBytes = CryptoJs.AES.decrypt(item.chat, "mes");
+                            const messageContent = decryptedBytes.toString(CryptoJs.enc.Utf8);
+                            const isSent = (item.from === user);
+                            let diffDate = false;
+                            if(index !== 0){
+                              if(chat[index-1].mDate !== item.mDate){
+                                diffDate = true
                               }
-                          </div>
-                      );
-                  })
-              }
-          </div>
-          <div className="sticky bottom-0 w-full shadow">
-              <div className="relative">
-                  <input onChange={(e)=>setRecentChat(e.target.value)} value={recentChat} placeholder="Type you message" className="w-full rounded-full border-2 p-2"/>
-                  <button onClick={(e)=>sendMessage(e)} className="absolute bottom-1 right-2 py-1 px-4 bg-green-600 text-white font-semibold rounded-full shadow-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition ease-in-out duration-200 w-1/7">Send</button>
-              </div>
-          </div>
-        </div>
-      </div>       
+                            }
+                            else{
+                              diffDate = true
+                            }
+                            return (
+                                <div key={item.id}> 
+                                    {
+                                      diffDate && 
+                                        <div>
+                                          <p className='text-center text-black'>{item.mDate}</p>
+                                        </div>
+                                    }
+                                    {
+                                        isSent ? 
+                                        <div className="w-full flex justify-end items-center w-full">
+                                            {/* <small className="text-xs font-light">you</small> */}
+                                            <div key={item.id} className='message message-sent ms-2'>
+                                                <p className="text-xl text-black font-medium">{messageContent}<small className="text-xs text-black font-light ms-2">{item.mTime}</small></p>
+                                            </div>
+                                            {
+                                              item.seen ?
+                                                <p className='text-blue-500'>&#10004;</p>
+                                              :
+                                                <p className='text-gray-500'>&#10004;</p>
+                                            }
+                                        </div>
+                                        :
+                                        <div className="w-full flex justify-start items-center">
+                                            <div key={item.id} className='message message-received  ms-2'>
+                                                <p className="text-xl text-black font-medium">{messageContent}<small className="text-xs text-black font-light ms-2">{item.mTime}</small></p>
+                                            </div>
+                                            {/* <small className="text-xs font-light ms-2">{opposite_person}</small> */}
+                                        </div>
+                                    }
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+                <div className="sticky z-50 bottom-0 w-full shadow">
+                    <div className="relative">
+                        <input onChange={(e)=>setRecentChat(e.target.value)} value={recentChat} placeholder="Type you message" className="w-full rounded-full border-2 p-2 text-black"/>
+                        <button onClick={(e)=>sendMessage(e)} className="absolute bottom-1 right-2 py-1 px-4 bg-green-600 text-white font-semibold rounded-full shadow-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition ease-in-out duration-200 w-1/7">Send</button>
+                    </div>
+                </div>
+            </div>
+        </div>       
     )
 }
